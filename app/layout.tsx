5837dc634ddd3b610d3742301982ace360b8f95c
ZBR <a799877006@qq.com>
2025-07-20 00:26:34 +0900

    暂时移除SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { AuthProvider } from "@/components/auth-provider"
 import { Toaster } from "@/components/ui/toaster"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,7 +26,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </AuthProvider>
-      <SpeedInsights />
+      {/* <SpeedInsights /> */}
       </body>
     </html>
   )
